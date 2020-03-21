5d20779262f2041530c3a89fe914ea61ef5c5743
Miles Olson <mpolson64@gmail.com>
2020-03-21 13:44:54 -0500

    final build configuration

--- _site/webpack.config.js
+++ _site/webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'scripts'),
+    path: path.resolve(__dirname, 'assets/js'),
   },
   module: {
     rules: [
